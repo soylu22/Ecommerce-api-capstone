717981c083a63bb7d4f38339e3e9587c8c81487e
Leul Misganaw <leulmisganaw222@gmail.com>
2025-12-11 16:28:10 -0500

    Day 1: Django project setup with core app

--- ecommerce_api_capstone/ecommerce_api_capstone/settings.py
+++ ecommerce_api_capstone/ecommerce_api_capstone/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core', #my backend app
+    'rest_framework', #third party
 ]
 
 MIDDLEWARE = [
