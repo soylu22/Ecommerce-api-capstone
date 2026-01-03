e75cb560336b8ce427856045bf52d19d5cf1938d
Leul Misganaw <leulmisganaw222@gmail.com>
2026-01-03 15:15:50 -0500

    Use environment variables for SECRET_KEY and DEBUG

--- ecommerce_api_capstone/ecommerce_api_capstone/settings.py
+++ ecommerce_api_capstone/ecommerce_api_capstone/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,11 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q$!x1y4cx--o_f%1ma0^1shx((uh8a_dwv(!rs1)$1(6qic@v@'
+SECRET_KEY = os.environ.get("SECRET_KEY", "unsafe-secret")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
+DEBUG = os.environ.get("DEBUG") == "True"
 ALLOWED_HOSTS = ["*"]
 
 
